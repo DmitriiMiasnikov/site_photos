e84cc54e2506948466dc8daa8c6ead9515390212
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-08-26 17:11:35 +0300

    main page add animation bu click

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './MainPage.module.scss';
+import classnames from 'classnames';
 const mainPhotos = [
     'https://img5.goodfon.ru/original/2560x1440/9/7c/chelsea-bridge-river-thames-london-england-most-chelsi-rek-1.jpg',
     'https://img5.goodfon.ru/original/2048x1365/5/a4/rossiia-kaliningrad-alla-poliakova.jpg',
@@ -8,16 +9,40 @@ const mainPhotos = [
     'https://img5.goodfon.ru/original/2560x1440/3/d3/piazza-del-campo-torre-del-mangia-siena-tuscany-italy-piatst.jpg'
 ]
 const MainPage = (props) => {
+    const [activePhoto, setActivePhoto] = useState(0);
+    const [shouldSlide, setshouldSlide] = useState(false);
+    const slidePhoto = async () => {
+        setshouldSlide(true)
+        await new Promise(() => setTimeout(() => {
+            setshouldSlide(false)
+            setActivePhoto(activePhoto + 1);
+        }, 500))
+    }
+    if (activePhoto === mainPhotos.length) setActivePhoto(0)
     return (
         <div className = {styles.mainPage}>
             <div className={styles.blockForPhoto}>
                 {
                     mainPhotos.map((el, i) => {
-                        return <div className = {`${styles.photo} ${styles.photo}-${i}`} 
+                        return <div className = {classnames(styles.photo, {
+                            [styles.active]: activePhoto === i,
+                            [styles.slide]: activePhoto === i && shouldSlide,
+                            [styles.hide]: i !== activePhoto && i !== activePhoto + 1 && activePhoto !== mainPhotos.length - 1 || 
+                                activePhoto === mainPhotos.length - 1 && i !== mainPhotos.length - 1 && i !== 0
+                         })}
                         style={{ 'background': `url(${el}) 50% center / cover no-repeat` }}
+                        onClick = {() => slidePhoto()}
+                        key = {i}
                         ></div>
                     })
                 }
+            </div>
+            <div className = {styles.description}>
+                <div className = {styles.innerBlock}>
+                <div className = {styles.name}>Dmitrii<br></br>Miasnikov</div>
+                <div className = {styles.proff}>Frontend</div>
+                </div>
+
             </div>
         </div>
     )
