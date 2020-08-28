74be32069cc377260807567cc33878f1dbf0a8ec
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-08-29 01:43:13 +0300

    remove containers, dont use them, and add 404 page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import styles from './App.module.scss';
-import Header from './components/Header/HeaderContainer';
-import Footer from './components/Footer/FooterContainer';
-import MainPage from './components/MainPage/MainPageContainer';
-import ArchPage from './components/ArchPage/ArchPageContainer';
-import NaturePage from './components/NaturePage/NaturePageContainer';
-import AboutMePage from './components/AboutmePage/AboutmePageContainer';
-import { Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import MainPage from './components/MainPage/MainPage';
+import ArchPage from './components/ArchPage/ArchPage';
+import NaturePage from './components/NaturePage/NaturePage';
+import AboutMePage from './components/AboutmePage/AboutmePage';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
       <div className={styles.main}>
-        <Route path={'/main'} render={() => <MainPage />} />
-        <Route path={'/nature'} render={() => <NaturePage />} />
-        <Route path={'/arch'} render={() => <ArchPage />} />
-        <Route path={'/aboutme'} render={() => <AboutMePage />} />
+        <Switch>
+          <Redirect exact from={'/'} to={'/main'} />
+          <Route exact path={'/main'} render={() => <MainPage />} />
+          <Route exact path={'/nature'} render={() => <NaturePage />} />
+          <Route exact path={'/arch'} render={() => <ArchPage />} />
+          <Route exact path={'/aboutme'} render={() => <AboutMePage />} />
+          <Route path={'*'} render={() =>
+            <div style={{ fontSize: 32, margin: '30px 0' }}>404 error, page not found</div>
+          } />
+        </Switch>
       </div>
       <Footer />
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import { NavLink } from 'react-router-dom';
-
-const Header = (props) => {
+const links = {
+    main: 'Main page',
+    nature: 'Nature',
+    arch: 'Architecture',
+    aboutme: 'About me'
+}
+const Header = () => {
     return (
         <div className={styles.header}>
             <div className = {styles.links}>
             {
-                Object.keys(props.links).map((el, i) => {
+                Object.keys(links).map((el, i) => {
                     return <NavLink to={`/${el}`} activeClassName={styles.active} className={styles.link} key={i}>
-                        {Object.values(props.links)[i]}
+                        {Object.values(links)[i]}
                     </NavLink>
                 })
             }
